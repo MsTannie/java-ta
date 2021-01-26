d2072ad14e9be131ab625e827b6e0e8cf6009b59
MsTannie <50020073+MsTannie@users.noreply.github.com>
2021-01-26 13:22:33 +0200

    Tanaka Daka
    she-code-cohort-4 application

---
+++ Palindrome code.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8/>
+<title>The string passed is a palindrome or not/title>
+</head>
+<body>
+<script>
+  function check_Palindrome(str_entry){
+// Change the string into lower case and remove  all non-alphanumeric characters
+   var cstr = str_entry.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+	var ccount = 0;
+// Check whether the string is empty or not
+	if(cstr==="") {
+		console.log("Nothing found!");
+		return false;
+	}
+// Check if the length of the string is even or odd 
+	if ((cstr.length) % 2 === 0) {
+		ccount = (cstr.length) / 2;
+	} else {
+// If the length of the string is 1 then it becomes a palindrome
+		if (cstr.length === 1) {
+			console.log("String is a palindrome.");
+			return true;
+		} else {
+// If the length of the string is odd ignore middle character
+			ccount = (cstr.length - 1) / 2;
+		}
+	}
+// Loop through to check the first character to the last character and then move next
+	for (var x = 0; x < ccount; x++) {
+// Compare characters and drop them if they do not match 
+		if (cstr[x] != cstr.slice(-1-x)[0]) {
+			console.log("Entry is not a palindrome.");
+			return false;
+		}
+	}
+	console.log("The string is a palindrome.");
+	return true;
+}
+</script>
+</body>
+</html>
